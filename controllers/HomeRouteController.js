bb912a79501b7aa1df243c21a83e4a07ed4a00e2
Asarvarjon <amriyevsarvarjon@yandex.com>
2021-11-13 17:25:26 +0500

    fixed

--- controllers/HomeRouteController.js
+++ controllers/HomeRouteController.js
@@ -4,11 +4,46 @@ module.exports = class  UsersController{
         try { 
 
             const news = await req.db.news.findAll({
-                raw: true
+                raw: true,
+                order:[['updatedAt', 'DESC']]
             }) 
 
+            const courses = await req.db.courses.findAll({
+                raw: true,
+                order:[['updatedAt', 'DESC']]
+            }) 
+
+            const scenes = await req.db.scenes.findAll({
+                raw: true,
+                order:[['updatedAt', 'DESC']]
+            }) 
+
+            const results = await req.db.results.findAll({
+                raw: true,
+                order:[['updatedAt', 'DESC']]
+            })
+
+            const comments = await req.db.comments.findAll({
+                raw: true,
+                order:[['updatedAt', 'DESC']]
+            })
+
+            const address = await req.db.address.findOne({
+                raw: true, 
+            }); 
+            // const teachers = await req.db.teachers.findAll({
+            //     raw: true,
+            //     order:[['updatedAt', 'DESC']]
+            // }) 
+
             res.render("index", { 
-                news
+                news,
+                courses,
+                scenes,
+                results,
+                comments,
+                address
+                // teachers
             })
             
         } catch (error) { 
--- modules/pg/init.js
+++ modules/pg/init.js
@@ -19,7 +19,7 @@ module.exports = async function init(db) {
     const acount = await db.address.count(0); 
     if(acount === 0){
         const adress = await db.address.create({
-            a_map: `<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48271.64865105821!2d69.0349640524582!3d40.872356108637575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae0cffaa77dbbb%3A0x74bfadbb12b99dcc!2z0JDQutC60YPRgNCz0LDQvSwg0KPQt9Cx0LXQutC40YHRgtCw0L0!5e0!3m2!1sru!2s!4v1633431104574!5m2!1sru!2s" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>`,
+            a_map: `https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d48271.64865105821!2d69.0349640524582!3d40.872356108637575!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x38ae0cffaa77dbbb%3A0x74bfadbb12b99dcc!2z0JDQutC60YPRgNCz0LDQvSwg0KPQt9Cx0LXQutC40YHRgtCw0L0!5e0!3m2!1sru!2s!4v1633431104574!5m2!1sru!2s`,
             a_location: `Toshkent shahri, Chilonzor tumani, Ustozlar ko’chasi, 13-uy`,
             a_phone: "+998 90 350 55 55",
             a_email: "apexedu@gmail.com",
